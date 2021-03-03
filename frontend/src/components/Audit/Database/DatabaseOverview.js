7396339d77af6b95cb4090e0cf0a7fb5c9ec8910
faizalyaacob <jalpejal74123@gmail.com>
2021-03-03 11:24:41 +0800

    Responsive Graph

--- frontend/src/components/Audit/Database/DatabaseOverview.js
+++ frontend/src/components/Audit/Database/DatabaseOverview.js
@@ -2,7 +2,7 @@ import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import { Container, Row, Col, ButtonGroup, ToggleButton, Breadcrumb } from 'react-bootstrap'
 import SideBar from '../SideBar/SideBar';
-import { Legend, Line, LineChart ,Bar, BarChart, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { Legend, Line, LineChart ,Bar, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 function DatabaseOverview(){
 
@@ -77,22 +77,24 @@ function DatabaseOverview(){
                             <Breadcrumb.Item active>Databases Overview</Breadcrumb.Item>
                         </Breadcrumb>
                         <Row>
-                            <Col>
+                            <Col fluid>
                                 <h4 style={{color:"white",marginBottom:"20px"}}>Total Queries And Their Average Speed</h4>
-                                <BarChart margin={{left:150,right:20, bottom:40}} layout="horizontal" width={1200} height={300} data={queriesnavgexec}>
-                                    <CartesianGrid vertical={false} horizontal={true} />
-                                    <XAxis tick={{ fontSize:"11.5px",fontWeight:"bold" }} stroke="white" type="category" dataKey="db" label={{ value: "Database",fill:"white", dy: 25}}/>
-                                    <YAxis yAxisId="left" tick={{ fontSize:"11.5px",fontWeight:"bold" }} stroke="white" type="number" dataKey="queries" label={{ value: "Queries",fill:"white", angle:270, dx:-25}} />
-                                    <YAxis yAxisId="right" orientation='right' tick={{ fontSize:"10.5px",fontWeight:"bold" }} stroke="white" type="number" dataKey="avgexectime" label={{ value: "Avg. Running Time (ms)",fill:"white", angle:90, dx:25}} />
-                                    <Tooltip />
-                                    <Legend verticalAlign="top" height={30} />
-                                    <Bar yAxisId="left" dataKey="queries" fill="#54C571" />
-                                    <Bar yAxisId="right" dataKey="avgexectime" fill="#8884d8" />
-                                </BarChart>  
+                                <ResponsiveContainer width="90%" height={300}>
+                                    <BarChart margin={{left:150,right:20, bottom:40}} layout="horizontal" data={queriesnavgexec}>
+                                        <CartesianGrid vertical={false} horizontal={true} />
+                                        <XAxis tick={{ fontSize:"11.5px",fontWeight:"bold" }} stroke="white" type="category" dataKey="db" label={{ value: "Database",fill:"white", dy: 25}}/>
+                                        <YAxis yAxisId="left" tick={{ fontSize:"11.5px",fontWeight:"bold" }} stroke="white" type="number" dataKey="queries" label={{ value: "Queries",fill:"white", angle:270, dx:-25}} />
+                                        <YAxis yAxisId="right" orientation='right' tick={{ fontSize:"10.5px",fontWeight:"bold" }} stroke="white" type="number" dataKey="avgexectime" label={{ value: "Avg. Running Time (ms)",fill:"white", angle:90, dx:25}} />
+                                        <Tooltip />
+                                        <Legend verticalAlign="top" height={30} />
+                                        <Bar yAxisId="left" dataKey="queries" fill="#54C571" />
+                                        <Bar yAxisId="right" dataKey="avgexectime" fill="#8884d8" />
+                                    </BarChart>  
+                                </ResponsiveContainer>   
                             </Col>
                         </Row>
                         <Row>
-                            <Col>
+                            <Col fluid>
                                 <h4 style={{color:"white",marginBottom:"20px"}}>Queries per database each day</h4>
                                 
                                 {uniqueValues.map((value, index) => (
@@ -108,24 +110,27 @@ function DatabaseOverview(){
                                             {value !== null && value}
                                             </ToggleButton>
                                         </ButtonGroup>
-
-                                        <LineChart key={index} margin={{left:150,right:20, bottom:40}} width={1200} height={300} data={filtering(query,value)}>
-                                            <CartesianGrid vertical={false}  />
-                                            <XAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="category" dataKey="date" label={{ value: "Day",fill:"white", dy: 25}} tickFormatter={formatXAxis}/>
-                                            <YAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="number" dataKey="queries" label={{ value: "Total Query",fill:"white", angle:270, dx:-25}} />
-                                            <Tooltip />                                  
-                                            <Line type="linear" dataKey="queries" strokeWidth={2} fill="#8884d8" dot={false} />
-                                        </LineChart>   
+                                        <ResponsiveContainer width="90%" height={300}>
+                                            <LineChart key={index} margin={{left:150,right:20, bottom:40}} width={1200} height={300} data={filtering(query,value)}>
+                                                <CartesianGrid vertical={false}  />
+                                                <XAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="category" dataKey="date" label={{ value: "Day",fill:"white", dy: 25}} tickFormatter={formatXAxis}/>
+                                                <YAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="number" dataKey="queries" label={{ value: "Total Query",fill:"white", angle:270, dx:-25}} />
+                                                <Tooltip />                                  
+                                                <Line type="linear" dataKey="queries" strokeWidth={2} fill="#8884d8" dot={false} />
+                                            </LineChart>   
+                                        </ResponsiveContainer>
                                     </div>
                                     
                                 ))}
-                                <LineChart margin={{left:150,right:20, bottom:40}} width={1200} height={300} data={query}>
-                                    <CartesianGrid vertical={false}  />
-                                    <XAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="category" dataKey="date" label={{ value: "Day",fill:"white", dy: 25}} tickFormatter={formatXAxis}/>
-                                    <YAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="number" dataKey="queries" label={{ value: "Total Query",fill:"white", angle:270, dx:-25}} />
-                                    <Tooltip />                                  
-                                    <Line type="linear" dataKey="queries" strokeWidth={2} fill="#8884d8" dot={false} />
-                                </LineChart>   
+                                <ResponsiveContainer width="90%" height={300}>
+                                    <LineChart margin={{left:150,right:20, bottom:40}} width={1200} height={300} data={query}>
+                                        <CartesianGrid vertical={false}  />
+                                        <XAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="category" dataKey="date" label={{ value: "Day",fill:"white", dy: 25}} tickFormatter={formatXAxis}/>
+                                        <YAxis tick={{ fontSize:"12px",fontWeight:"bold" }} stroke="white" type="number" dataKey="queries" label={{ value: "Total Query",fill:"white", angle:270, dx:-25}} />
+                                        <Tooltip />                                  
+                                        <Line type="linear" dataKey="queries" strokeWidth={2} fill="#8884d8" dot={false} />
+                                    </LineChart> 
+                                </ResponsiveContainer>  
                             </Col>
                         </Row>
                     </Col>
