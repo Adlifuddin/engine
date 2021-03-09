8ef96788d584d32772bf6b50e97491a0689f1c52
Nasuhanina <nasuhanina.3797@gmail.com>
2021-03-09 12:23:05 +0800

    modify people modal

--- frontend/src/components/People/AddSomeoneModal.js
+++ frontend/src/components/People/AddSomeoneModal.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 import { Button, Modal, Form } from 'react-bootstrap'
+import Select from 'react-select'
+
 
 function AddSomeoneModal(props) {
+
+    const[listGroup, setListGroup] = useState([])
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:5000/api/people/listgroups")
+        .then(res => {
+            console.log(res.data)
+            setListGroup(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })    
+    }, [])
+
+    const options = listGroup.map(data => ({value:data.name,label:data.name}))
+
     return (
         <div>
             <Modal
@@ -30,6 +50,11 @@ function AddSomeoneModal(props) {
                     <Form.Group controlId="formEmail">
                         <Form.Label>Email</Form.Label>
                         <Form.Control required type="email" placeholder="youlooknicetoday@email.com" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formUserGroups">
+                        <Form.Label>Groups</Form.Label>
+                        <Select isMulti={true} options={options} required type="usergroups" />
                     </Form.Group>
 
                     <Button className="float-right" variant="primary" type="submit">
