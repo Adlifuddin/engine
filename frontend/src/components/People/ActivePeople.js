a974eab392486bd43f0c2fb0d222cbb08a71b9d2
faizalyaacob <jalpejal74123@gmail.com>
2021-03-09 14:47:00 +0800

    Added Deactivate user modal

--- frontend/src/components/People/ActivePeople.js
+++ frontend/src/components/People/ActivePeople.js
@@ -2,9 +2,10 @@ import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import { Table , Button, DropdownButton, Dropdown} from 'react-bootstrap'
 import Select from 'react-select'
-import { FaEllipsisH } from 'react-icons/fa';
-import EditUserModal from './EditUserModal';
-import ResetPasswordModal from './ResetPasswordModal';
+import { FaEllipsisH } from 'react-icons/fa'
+import EditUserModal from './Modal/EditUserModal'
+import ResetPasswordModal from './Modal/ResetPasswordModal'
+import DeactivateUserModal from './Modal/DeactivateUserModal'
 
 function ActivePeople(){
 
@@ -12,6 +13,7 @@ function ActivePeople(){
     const[listGroup, setListGroup] = useState([])
     const [EditUserModalShow, setEditUserModalShow] = useState(false)
     const [ResetPasswordModalShow, setResetPasswordModalShow] = useState(false)
+    const [DeactivateUserModalShow, setDeactivateUserModalShow] = useState(false)
 
     useEffect(() => {
         axios
@@ -83,7 +85,8 @@ function ActivePeople(){
                                                     <EditUserModal show={EditUserModalShow} onHide={() => setEditUserModalShow(false)} />
                                                     <Dropdown.Item onClick={() => setResetPasswordModalShow(true)}>Reset password</Dropdown.Item>
                                                     <ResetPasswordModal show={ResetPasswordModalShow} onHide={() => setResetPasswordModalShow(false)} />
-                                                    <Dropdown.Item href="#/action-3">Deactivate user</Dropdown.Item>
+                                                    <Dropdown.Item onClick={() => setDeactivateUserModalShow(true)}>Deactivate user</Dropdown.Item>
+                                                    <DeactivateUserModal show={DeactivateUserModalShow} onHide={() => setDeactivateUserModalShow(false)} />
                                                 </Dropdown.Menu>
                                             </Dropdown>
                                         </td>
